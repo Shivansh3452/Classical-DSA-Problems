f52e82ccd6f2812a8d5f6a9f6e34df0406b1cbaf
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-10 11:02:00 +0530

    Implement constant space Fibonacci function
    Added constant space and time complexity implementation for Fibonacci.

--- fibonacci_dp.cpp
+++ fibonacci_dp.cpp
@@ -33,6 +33,23 @@ int fib(int n) {
     }
 */
 
+/*
+Constant Space and Time Comoplexity
+int fib(int n) {
+        if(n<=1)
+            return n;
+        int c;
+        int a=0;
+        int b=1;
+        for(int i=1;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+*/
+
 int main(){
     int n;
     cin>>n; //size of vector
@@ -42,3 +59,4 @@ int main(){
 
 
 }
+
