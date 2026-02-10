fb3de1c82dee141c9ae8872e01c352138c4a3f05
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-10 10:55:10 +0530

    Fibonacci implementation add Bottom Up approach
    Added comments for top-down approach and included a bottom-up approach as a commented-out section.

--- fibonacci_dp.cpp
+++ fibonacci_dp.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-
+//Top Down
 int solver(int n,vector<int> &dp){
     if(n<=1)
         return n;
@@ -17,11 +17,28 @@ int fib(int n) {
     return solver(n,dp);
 }
 
+/*
+Bottom up
+int fib(int n) {
+
+        if(n<=1)
+            return n;
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+*/
+
 int main(){
     int n;
     cin>>n; //size of vector
     int ans=fib(n);
     cout<<ans<<endl;
     return 0;
+
 
 }
